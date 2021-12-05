1b08e54c05a61f9f889f7f84903aaf30c710a15e
isubasinghe <isitha@pipeki.io>
2021-12-05 01:18:45 -0500

    Formatted code

--- src/Vector.hs
+++ src/Vector.hs
@@ -1,25 +1,29 @@
-{-# LANGUAGE GADTs, KindSignatures, DataKinds, TypeFamilies, UndecidableInstances #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
-module Vector where 
+module Vector where
 
-data Nat = Zero | Succ Nat deriving(Show)
+data Nat = Zero | Succ Nat deriving (Show)
 
-data Vector (n :: Nat) (a :: *) where 
-  VNil  :: Vector 'Zero a
+data Vector (n :: Nat) (a :: *) where
+  VNil :: Vector 'Zero a
   VCons :: a -> Vector n a -> Vector ('Succ n) a
 
-instance Show a => Show (Vector n a) where 
+instance Show a => Show (Vector n a) where
   show VNil = "VNil"
   show (VCons a as) = "VCons[" ++ show a ++ "](" ++ show as ++ ")"
 
-add :: Nat -> Nat -> Nat 
+add :: Nat -> Nat -> Nat
 add Zero n = n
 add (Succ n) m = add n (Succ m)
 
-type family Add n m where 
+type family Add n m where
   Add 'Zero n = n
   Add ('Succ n) m = 'Succ (Add n m)
 
-append :: Vector n a -> Vector m a -> Vector (Add n m) a 
+append :: Vector n a -> Vector m a -> Vector (Add n m) a
 append VNil xs = xs
 append (VCons a rest) xs = VCons a (append rest xs)
