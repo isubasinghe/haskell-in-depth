2cf447c02cdaf048ba0afad5d1b8d010c28f2f46
isubasinghe <isubasinghe@student.unimelb.edu.au>
2021-12-05 01:42:59 -0500

    Format

--- src/CH5.hs
+++ src/CH5.hs
@@ -1,21 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ViewPatterns #-}
+
 module CH5 where
+
 import Control.Monad.Writer
 import qualified Data.Text as T
 
-type SQL = T.Text 
+type SQL = T.Text
 
 data ErrorMsg = WrongFormat Int T.Text
   deriving (Show)
 
-
 genSQL :: T.Text -> Writer [ErrorMsg] SQL
-genSQL txt = T.concat <$> traverse processLine (zip [1..] $ T.lines txt)
+genSQL txt = T.concat <$> traverse processLine (zip [1 ..] $ T.lines txt)
 
 genInsert :: T.Text -> T.Text -> SQL
-genInsert s1 s2 = 
-    "INSERT INTO items VALUES ('" <> s1 <> "','" <> s2 <> ",);\n"
+genInsert s1 s2 =
+  "INSERT INTO items VALUES ('" <> s1 <> "','" <> s2 <> ",);\n"
 
 processLine :: (Int, T.Text) -> Writer [ErrorMsg] SQL
 processLine (_, T.splitOn ":" -> [s1, s2]) = pure $ genInsert s1 s2
